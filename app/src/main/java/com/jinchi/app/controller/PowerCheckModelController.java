aa9936024fae5992f6c67efbd3c318514c060efb
liutaoyi <2580295193@qq.com>
2020-03-26 21:55:15 +0800

    动力点检

--- app/src/main/java/com/jinchi/app/controller/PowerCheckModelController.java
+++ app/src/main/java/com/jinchi/app/controller/PowerCheckModelController.java
@@ -1,5 +1,6 @@
 package com.jinchi.app.controller;
 
+import com.jinchi.app.dto.IdDto;
 import com.jinchi.app.dto.Result;
 import com.jinchi.app.service.PowerCheckModelService;
 import com.jinchi.app.utils.ResultUtil;
@@ -7,8 +8,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,8 +27,8 @@ public class PowerCheckModelController {
 
     @PostMapping(value = "detail")
     @ApiOperation(value = "详情")
-    public Result detail(@RequestParam Long id) {
-        return ResultUtil.success(modelService.detail(id));
+    public Result detail(@RequestBody IdDto dto) {
+        return ResultUtil.success(modelService.detail(dto.getId()));
     }
 
 }
