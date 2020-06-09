b2f24a3f5964a36e446e48d36b6ff743315ea7c9
WZH9606 <1192573254@qq.com>
2020-06-09 10:46:47 +0800

    原材料

---
+++ app/src/main/java/com/jinchi/app/mapper/CommonBatchNumberChildMapper.java
@@ -0,0 +1,59 @@
+package com.jinchi.app.mapper;
+
+import com.jinchi.app.domain.CommonBatchNumberChild;
+import org.apache.ibatis.annotations.Param;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author XudongHu
+ * @className CommonBatchNumberChildMapper
+ * @apiNote TODO
+ * @modifer
+ * @since 2019/10/23 0:47
+ */
+@Mapper
+@Component
+public interface CommonBatchNumberChildMapper {
+    /**
+     * 新增
+     * @param commonBatchNumberChild
+     * @return
+     */
+    int insert(CommonBatchNumberChild commonBatchNumberChild);
+
+    /**
+     * 查询
+     * @param commonBatchNumberChild
+     * @return
+     */
+    CommonBatchNumberChild selectByExample(CommonBatchNumberChild commonBatchNumberChild);
+
+    /**
+     * 根据批号id查询
+     * @param batchNumberId
+     * @return
+     */
+    CommonBatchNumberChild selectByBatchNumberId(@Param("batchNumberId") Integer batchNumberId);
+
+    /**
+     * 根据子批号id查询
+     * @param childId
+     * @return
+     */
+    CommonBatchNumberChild selectByChildId(@Param("childId") Integer childId);
+
+    /**
+     * 删除
+     * @param batchNumberId
+     * @return
+     */
+    int deleteByBatchNumberId(@Param("batchNumberId") Integer batchNumberId);
+
+    /**
+     * 删除
+     * @param childId
+     * @return
+     */
+    int deleteByChildId(@Param("childId") Integer childId);
+}
